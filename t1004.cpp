a4e1868481c306bfa31a0978967ee06a78a27a94
KADGang <54164637+KADGang@users.noreply.github.com>
2020-01-19 21:55:55 +0800

    Create t1004.cpp

---
+++ t1004.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+	cin >> a >> b >> c;
+	printf("%8d\n%8d\n%8d\n",a,b,c);
+	return 0;
+}
+
+//1004 水题，老活，格式化输入输出。
